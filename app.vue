bbe97d3f5b8b5c50a56bc52647bce060e6e98349
KimHS <kkspooto@gmail.com>
2025-05-21 12:18:20 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,10 +3,27 @@
     <!-- 네비게이션 바 -->
     <nav class="navbar">
       <ul class="nav-menu">
-        <li><NuxtLink to="/" @click="selectMenu('API')">API</NuxtLink></li>
-        <li><NuxtLink to="/" @click="selectMenu('Fonts')">Fonts</NuxtLink></li>
-        <li><NuxtLink to="/" @click="selectMenu('CSS Code')">CSS Code</NuxtLink></li>
-        <li><NuxtLink to="/" @click="selectMenu('Free')">Free Board</NuxtLink></li>
+        <li>
+          <NuxtLink
+            to="/"
+            @click="selectMenu('API')"
+            :class="{ 'active': currentMenu === 'API' }"
+          >API</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/"
+            @click="selectMenu('Fonts')"
+            :class="{ 'active': currentMenu === 'Fonts' }"
+          >Fonts</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/"
+            @click="selectMenu('CSS Code')"
+            :class="{ 'active': currentMenu === 'CSS Code' }"
+          >CSS Code</NuxtLink>
+        </li>
       </ul>
     </nav>
 
@@ -23,9 +40,7 @@ export default {
     const currentMenu = ref('API');
 
     const selectMenu = (menu) => {
-      if (menu !== 'Free') {
-        currentMenu.value = menu;
-      }
+      currentMenu.value = menu;
     };
 
     return {
@@ -59,6 +74,9 @@ export default {
 .nav-menu li a {
   font-size: min(5vw, 24px);
   text-decoration: none;
-  color: #40ed21;
+  color: white; /* 기본 색상: 흰색 */
+}
+.nav-menu li a.active {
+  color: #40ed21; /* 선택된 메뉴 색상: 초록색 */
 }
 </style>
